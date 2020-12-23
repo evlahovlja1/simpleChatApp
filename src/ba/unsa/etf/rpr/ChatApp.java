48ea509f9c797292955ed7783daf98f4fba10bfe
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-12-23 22:50:06 +0100

    gotova aplikacija

--- src/ba/unsa/etf/rpr/ChatApp.java
+++ src/ba/unsa/etf/rpr/ChatApp.java
@@ -7,15 +7,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ChatApp extends Application {
+    ChatAppController kontroler;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        ChatAppController kontroler = new ChatAppController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+        loader.setController(kontroler);
+        this.kontroler = kontroler;
+        kontroler.createConnection();
+
+        Parent root = loader.load();
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
+
+    @Override
+    public void stop() throws Exception {
+        this.kontroler.closeConnection();
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/ba/unsa/etf/rpr/ChatAppController.java
+++ src/ba/unsa/etf/rpr/ChatAppController.java
@@ -1,4 +1,53 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ChatAppController {
+    @FXML private TextArea messages;
+    @FXML private TextField input;
+    //false za klijenta i rerun app
+    private boolean isServer = false;
+    private NetworkConnection connection = isServer ? createServer() : createClient();
+
+
+    private Server createServer() {
+        return new Server(data -> {
+            Platform.runLater(() -> {
+                messages.appendText(data.toString() + "\n");
+            });
+        }, 55555);
+    }
+
+    private Client createClient() {
+        return new Client(data -> {
+            Platform.runLater(() -> {
+                messages.appendText(data.toString() + "\n");
+            });
+        }, "127.0.0.1", 55555);
+    }
+
+
+    public void createConnection() throws Exception {
+        connection.startConnection();
+    }
+
+    public void closeConnection() throws Exception {
+        connection.closeConnection();
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+        String message = isServer ? "Server: " : "Client: ";
+        message += input.getText();
+        input.clear();
+        messages.appendText(message + "\n");
+        try {
+            connection.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
